ff2aaed1027de9de84af447d48830a4cc8475446
kvyus <215489811+kvyus@users.noreply.github.com>
2025-12-26 23:26:14 +0200

    fix grub options

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,6 +11,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
   

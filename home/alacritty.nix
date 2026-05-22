96baf95bc2e0f9bf57095425bcc5b6e23378f7da
kvyus <215489811+kvyus@users.noreply.github.com>
2026-05-22 18:14:13 +0300

    delete font settings

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -8,27 +8,6 @@
             x = 5;
             y = 5;
          }; 
-#         font.normal = {
-#            family = "JetBrainsMono Nerd Font";
-#            style = "Regular";
-#         };
-
-#         font.bold = {
-#            family = "JetBrainsMono Nerd Font";
-#            style = "Bold";
-#         };
-
-#         font.italic = {
-#            family = "JetBrainsMono Nerd Font";
-#            style = "Italic";
-#         };
-
-#         font.bold_italic = {
-#            family = "JetBrainsMono Nerd Font";
-#            style = "Bold Italic";
-#         };
-
-#         font.size = 13.0;
       };
    };
 }

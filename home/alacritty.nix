605282359e845d424a46cbe822f5b96d8ed9e4ee
kvyus <215489811+kvyus@users.noreply.github.com>
2026-05-26 16:35:44 +0300

    opacity

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -1,13 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
    programs.alacritty = {
       enable = true;
       theme = "gruvbox_dark";
       settings = {
-         window.padding = {
-            x = 5;
-            y = 5;
-         }; 
+         window = { 
+            padding = {
+               x = 5;
+               y = 5;
+            }; 
+            opacity = lib.mkForce 0.8;
+         };
       };
    };
 }
